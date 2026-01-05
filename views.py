7c7a7681f67d4ff6d14f4dca72e41f9225c8991d
aashish1931 <ashishdarji109@gmail.com>
2026-01-05 20:08:11 +0530

    qr code png image created

--- views.py
+++ views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render,redirect
 from .forms import QRCodeForm
-
+import qrcode
 def generate_qr_code(request):
     if request.method=='POST':
         form=QRCodeForm(request.POST)
         if form.is_valid():
             res_name=form.cleaned_data['restaurant_name']
             url=form.cleaned_data['url']
-            print(res_name,url)
+
+            #generate qr code
+            qr=qrcode.make(url)
+           # print(qr)#<qrcode.image.pil.PilImage object at 0x0000015D8D228D70> qr code
+            file_name=res_name.replace(" ","_").lower() + "_menu.png"
+            qr.save(file_name)
     else:
         form=QRCodeForm()
         context={
