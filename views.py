13786d4a23ac4d364af7c2d91e486c54185ccc47
aashish1931 <ashishdarji109@gmail.com>
2026-01-05 19:59:48 +0530

    form data submitted success

--- views.py
+++ views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import QRCodeForm
 
 def generate_qr_code(request):
-    form=QRCodeForm()
-    context={
-        'form':form,
-    }
-    return render(request,'generate_qr_code.html',context)
+    if request.method=='POST':
+        form=QRCodeForm(request.POST)
+        if form.is_valid():
+            res_name=form.cleaned_data['restaurant_name']
+            url=form.cleaned_data['url']
+            print(res_name,url)
+    else:
+        form=QRCodeForm()
+        context={
+            'form':form,
+        }
+        return render(request,'generate_qr_code.html',context)
     
