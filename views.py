32d12e7eef6d6df7b8c8fc10c7000a899d551e6a
aashish1931 <ashishdarji109@gmail.com>
2026-01-05 19:50:21 +0530

    form is created

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .forms import QRCodeForm
+
+def generate_qr_code(request):
+    form=QRCodeForm()
+    context={
+        'form':form,
+    }
+    return render(request,'generate_qr_code.html',context)
+    
